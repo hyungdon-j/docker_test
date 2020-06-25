13e4c01935827e5d276534594fecd2511a365109
hyungdon <joojoo0513@gmail.com>
2020-06-25 11:10:45 +0900

    git test

---
+++ do.sh
@@ -0,0 +1,14 @@
+for i in $(seq 1 100);
+    do
+        # mkdir $i
+        if [ $i -eq 3 ]; then
+            echo $i
+        fi
+        # docker run -it --name uu$i ubuntu
+    done
+
+# for i in $(cat data.txt);
+#     do
+#         # mkdir $i
+#         echo ------ $i ------
+#     done
